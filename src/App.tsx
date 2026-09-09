0456b2dfc464aa58167be02c39b8afc7c92eef9b
Debjani <debjani6ghosh@gmail.com>
2026-09-09 13:46:11 +0530

    feat: add initial project setup with React, TypeScript, and Vite
    - Created main entry point for the application with React rendering.
    - Configured TypeScript with separate configurations for app and node.
    - Set up Vite configuration for React plugin.
    - Added global CSS file for styling.
    - Consume the React ui-kit library

---
+++ src/App.tsx
@@ -0,0 +1,64 @@
+import {
+  // LoginPage,
+  // type Credentials,
+  type RSACredentials,
+  RSALoginPage,
+} from "ui-kit";
+import productIcon from "./assets/new_logo.svg";
+
+function App() {
+  // const selectOptions: { label: string; value: string }[] = [
+  //   { label: "This BMC UI", value: "bmc_ui" },
+  //   { label: "LDAP", value: "ldap" },
+  //   { label: "Active Directory", value: "active_dir" },
+  // ];
+
+  // const links = [
+  //   { label: "Help", href: "https://www.dell.com/support/home" },
+  //   {
+  //     label: "Drivers & Downloads",
+  //     href: "https://www.dell.com/support/home/en-us?app=drivers",
+  //   },
+  //   {
+  //     label: "Manuals",
+  //     href: "https://www.dell.com/support/home/en-us?app=manuals",
+  //   },
+  //   { label: "TechCenter", href: "https://developer.dell.com" },
+  // ];
+
+  // const handleLogin = (credentials: Credentials) => {
+  //   console.log(credentials);
+  // };
+
+  const handleRSALogin = (credentials: RSACredentials) => {
+    console.log(credentials);
+  };
+
+  const onCancel = () => {
+    console.log("action canceled");
+  };
+
+  return (
+    <div>
+      {/* <LoginPage
+        productIcon={<img src={productIcon} alt="product icon" />}
+        productTitle="Server BMC GUI"
+        productSubtitle="Server Hostname | Server Model | License Type"
+        domainOptions={selectOptions}
+        onSubmit={handleLogin}
+        linksArray={links}
+        miscellaneousMsg="Security Notice: Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+        "
+      /> */}
+      <RSALoginPage
+        productIcon={<img src={productIcon} alt="product icon" />}
+        productTitle="Server BMC GUI"
+        onSubmit={handleRSALogin}
+        onCancel={onCancel}
+        miscellaneousMsg="Copyright Notice: Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+      />
+    </div>
+  );
+}
+
+export default App;
